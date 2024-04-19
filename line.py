11a6874bc2c4dad0235923bec427dfd5d9e7f2a2
julietaahernandez <162722017+julietaahernandez@users.noreply.github.com>
2024-04-18 22:49:19 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,6 +1,6 @@
 import math
 def line():
-    
+    print("TO DO")
     coeficiente_A = float(input("Ingrese el coeficiente A: "))
     coeficiente_B = float(input("Ingrese el coeficiente B: "))
     coeficiente_X1 = float(input("Ingrese el coeficiente X1: "))
